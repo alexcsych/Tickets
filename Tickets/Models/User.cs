77f4ba6595bf686c75654611b4bc6768eda2c18c
alexcsych <alexsejsi4@gmail.com>
2026-04-06 17:20:13 +0300

    feat(models): add User entity with roles

---
+++ Tickets/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tickets.Models
+{
+    public enum UserRole
+    {
+        Customer,
+        Admin
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public UserRole Role { get; set; }
+    }
+}
