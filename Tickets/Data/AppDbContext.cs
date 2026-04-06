d8249b456e6aa2d089f0838de9433b9b28ffa5eb
alexcsych <alexsejsi4@gmail.com>
2026-04-06 17:14:07 +0300

    Add AppDbContext

---
+++ Tickets/Data/AppDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tickets.Models;
+
+namespace Tickets.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=tickets.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    Id = 1,
+                    Name = "Admin",
+                    LastName = "System",
+                    Email = "admin@tickets.com",
+                    Password = "admin_password",
+                    Role = UserRole.Admin
+                },
+                new User
+                {
+                    Id = 2,
+                    Name = "User",
+                    LastName = "User",
+                    Email = "User@mail.com",
+                    Password = "user_password",
+                    Role = UserRole.Customer
+                }
+            );
+        }
+    }
+}
