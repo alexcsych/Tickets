f4af0a7e057475d463a7931114fafa3474cab7f6
alexcsych <alexsejsi4@gmail.com>
2026-04-07 15:42:06 +0300

    feat(db): add seed data for buses and routes

--- Tickets/Data/AppDbContext.cs
+++ Tickets/Data/AppDbContext.cs
@@ -9,6 +9,9 @@ namespace Tickets.Data
     public class AppDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Bus> Buses { get; set; }
+        public DbSet<Route> Routes { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -35,6 +38,50 @@ namespace Tickets.Data
                     Email = "User@mail.com",
                     Password = "user_password",
                     Role = UserRole.Customer
+                }
+            );
+
+            modelBuilder.Entity<Bus>().HasData(
+                new Bus { Id = 1, Model = "Mercedes-Benz Sprinter", Number = "AP 1234 BT", Capacity = 18 },
+                new Bus { Id = 2, Model = "Setra S 515 HD", Number = "AA 7777 XX", Capacity = 25 }
+            );
+
+            modelBuilder.Entity<Route>().HasData(
+                new Route
+                {
+                    Id = 1,
+                    From = "Запоріжжя",
+                    To = "Київ",
+                    DepartureTime = new DateTime(2026, 5, 10, 08, 00, 00),
+                    Price = 850.00m,
+                    BusId = 1
+                },
+                new Route
+                {
+                    Id = 2,
+                    From = "Київ",
+                    To = "Львів",
+                    DepartureTime = new DateTime(2026, 5, 11, 14, 30, 00),
+                    Price = 600.00m,
+                    BusId = 2
+                },
+                new Route
+                {
+                    Id = 3,
+                    From = "Київ",
+                    To = "Запоріжжя",
+                    DepartureTime = new DateTime(2026, 5, 21, 12, 30, 00),
+                    Price = 800.00m,
+                    BusId = 2
+                },
+                new Route
+                {
+                    Id = 4,
+                    From = "Запоріжжя",
+                    To = "Львів",
+                    DepartureTime = new DateTime(2026, 5, 21, 18, 00, 00),
+                    Price = 1300.00m,
+                    BusId = 1
                 }
             );
         }
